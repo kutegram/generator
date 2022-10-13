8d342ae6af849d0c3962da9f1c6ce0c26863e4eb
Mathew <me@crx.moe>
2022-10-13 19:22:49 +0300

    Update schema version

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char *argv[])
 {
     generate(":/mtproto.json", "MT", 0);
-    generate(":/telegram.json", "TL", 139);
+    generate(":/telegram.json", "TL", 145);
     qDebug("Finished.");
 }
