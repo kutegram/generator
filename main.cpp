d7122051acb95e3d35639ffc521dd0686ac890db
Mathew <me@crx.moe>
2023-04-30 17:57:45 +0300

    Forgot to bump version in code

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char *argv[])
 {
     generate(":/mtproto.json", "MT", 0, "tgstream.h");
-    generate(":/telegram.json", "TL", 145, "tgstream.h");
+    generate(":/telegram.json", "TL", 152, "tgstream.h");
     qDebug("Finished.");
 }
