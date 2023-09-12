9b4beb9510938cd30120532921a7c9116e78893f
Mathew <me@crx.moe>
2023-09-12 18:24:38 +0300

    Bump schema to layer 158

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char *argv[])
 {
     generate(":/mtproto.json", "MT", 0, "tgstream.h");
-    generate(":/telegram.json", "TL", 152, "tgstream.h");
+    generate(":/telegram.json", "TL", 158, "tgstream.h");
     qDebug("Finished.");
 }
